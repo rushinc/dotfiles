b0aed7e0aedc3c6cee395b834347c1c5c14ff232
Rushin Contractor <rushin@berkeley.edu>
2019-10-24 19:01:04 -0700

    Vim config for matlab.

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -5,3 +5,4 @@ if status --is-interactive
 end
 
 alias matlab='env _JAVA_AWT_WM_NONREPARENTING=1 matlab -nodesktop -nosplash'
+alias comsol='env GTK_PATH=/usr/lib/gtk-2.0 GTK2_RC_FILES=/home/rushin/.gtkrc-2.0_light comsol'
