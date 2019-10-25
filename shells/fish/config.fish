4eb536e2f1ed9d0344b437ed4da95ac7378defdc
Rushin Contractor <rushin@berkeley.edu>
2019-10-24 18:38:42 -0700

    Better theme + light variant.

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -3,3 +3,5 @@ if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
     source "$BASE16_SHELL/profile_helper.fish"
 end
+
+alias matlab='env _JAVA_AWT_WM_NONREPARENTING=1 matlab -nodesktop -nosplash'
