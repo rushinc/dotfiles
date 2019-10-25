d79ad95dabb3b31850ac82b23995f489ea055ed3
Rushin Contractor <rushin@berkeley.edu>
2019-10-25 00:08:20 -0700

    First merge attempt.

--- apps/config/polybar/launch.sh
+++ apps/config/polybar/launch.sh
@@ -6,8 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar top &
-# polybar bottom &
+# Launch polybar with hostname specific config
+case $HOSTNAME in
+	archPad)
+		polybar -c /home/rushin/.config/polybar/config_hidpi top &
+		;;
+	*)
+		polybar -c /home/rushin/.config/polybar/config_default top &
+		;;
+esac
 
 echo "Polybar launched..."
