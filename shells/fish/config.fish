36de772c8b315f0d9a9bb4b50bb2a009ca82b20a
Rushin Contractor <rushin@berkeley.edu>
2019-10-29 18:52:22 -0700

    Tune-up.

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -4,5 +4,7 @@ if status --is-interactive
     source "$BASE16_SHELL/profile_helper.fish"
 end
 
+set fish_greeting
+
 alias matlab='env _JAVA_AWT_WM_NONREPARENTING=1 matlab -nodesktop -nosplash'
 alias comsol='env GTK_PATH=/usr/lib/gtk-2.0 GTK2_RC_FILES=/home/rushin/.gtkrc-2.0_light comsol'
