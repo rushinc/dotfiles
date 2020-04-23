3fd163a548b607ccc2a53af595e66026d1374067
Rushin Contractor <rushin@berkeley.edu>
2020-04-22 22:34:52 -0700

    More aliases.

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -6,5 +6,7 @@ end
 
 set fish_greeting
 
+alias vim='vim --servername gvim'
 alias matlab='cd $HOME/Documents/MATLAB && env _JAVA_AWT_WM_NONREPARENTING=1 matlab -nodesktop -nosplash'
 alias comsol='env GTK_PATH=/usr/lib/gtk-2.0 GTK2_RC_FILES=/home/rushin/.gtkrc-2.0_light comsol'
+alias ls='lsd'
